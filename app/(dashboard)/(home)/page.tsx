7ca49357431f1cfd4c8510b5546c0585cb106718
KoyalkarAditya <koyalkaraditya123@gmail.com>
2024-11-08 13:25:46 +0530

    init project and desktop sidebar

---
+++ app/(dashboard)/(home)/page.tsx
@@ -0,0 +1,7 @@
+type Props = {};
+
+const HomePage = ({}: Props) => {
+  return <div>Home</div>;
+};
+
+export default HomePage;
---
+++ components/SideBar.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import {
+  CoinsIcon,
+  HomeIcon,
+  Layers2Icon,
+  ShieldCheckIcon,
+} from "lucide-react";
+import Logo from "./Logo";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { usePathname } from "next/navigation";
+
+const routes = [
+  {
+    href: "",
+    label: "Home",
+    icon: HomeIcon,
+  },
+  {
+    href: "workflows",
+    label: "Workflows",
+    icon: Layers2Icon,
+  },
+  {
+    href: "credentials",
+    label: "Credentials",
+    icon: ShieldCheckIcon,
+  },
+  {
+    href: "billing",
+    label: "Billing",
+    icon: CoinsIcon,
+  },
+];
+
+const DesktopSideBar = () => {
+  const pathname = usePathname();
+  const activeRoute =
+    routes.find(
+      (route) => route.href.length > 0 && pathname.includes(route.href)
+    ) || routes[0];
+  return (
+    <div className="hidden relative md:block max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 dark:text-foreground text-muted-foreground border-r-2 border-separate">
+      <div className="flex items-center justify-center gap-2 border-b-[1px] border-separate p-4">
+        <Logo />
+      </div>
+      <div className="p-2">Todo credits</div>
+      <div className="flex flex-col p-2 ">
+        {routes.map((route) => (
+          <Link
+            className={buttonVariants({
+              variant:
+                activeRoute.href == route.href
+                  ? "sidebarActiveItem"
+                  : "sidebarItem",
+            })}
+            key={route.href}
+            href={route.href}
+          >
+            <route.icon size={20} />
+            {route.label}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DesktopSideBar;
+
+//19.48
